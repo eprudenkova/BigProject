1441f7ab5016933a3e3bde74908c0d011b2f2c9a
Evgeniya Prudenkova <evgeniya@MacBook-Pro-16.local>
2021-01-27 20:34:57 -0500

    Solar system is ready to be deployed

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -3,5 +3,10 @@ package com.syntax;
 public class Main {
 
     public static void main(String[] args) {
+
+        SolarSystem solarSystem = new SolarSystem();
+        solarSystem.sun = 1;
+        solarSystem.moon = 1;
+        solarSystem.planets = 2;
     }
 }
